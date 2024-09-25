6321523454476b334fcd63ca11fbd1825ffe762f
sowad69 <abdullahalsowad326@gmail.com>
2024-09-25 15:06:04 +0600

    refactoring- remove duplocate interfaces

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,7 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatform";
-import { Platform } from "../hooks/useGames";
+import usePlatforms, { Platform } from "../hooks/usePlatform";
 
 interface Props {
   onSelectplatform: (platform: Platform) => void;
