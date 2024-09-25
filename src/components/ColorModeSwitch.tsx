baf680bd7117153612286fe89ae2f52704d840ee
sowad69 <abdullahalsowad326@gmail.com>
2024-09-25 14:56:40 +0600

    refactoring fetch game in react query

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -2,7 +2,7 @@ import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
-
+  //hello
   return (
     <HStack>
       <Switch
