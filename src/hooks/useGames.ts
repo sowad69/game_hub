7d75acc537c05681bb521fbb8465c0c496f7b396
sowad69 <abdullahalsowad326@gmail.com>
2024-09-27 17:43:47 +0600

    Refector- manage state with Zustand

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
-import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import useGameQueryStore from "../store";
 
 const apiClient = new APIClient<Game>("/games")
 
@@ -15,9 +15,10 @@ export interface Game {
     rating_top: number; 
   }
 
-const useGames=(gameQuery: GameQuery)=>
+const useGames=()=>{
 
-  useInfiniteQuery<FetchResponse<Game>, Error>({
+  const gameQuery = useGameQueryStore(s=> s.gameQuery);
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: ({pageParam= 1}) => 
       apiClient.getAll({
@@ -34,6 +35,8 @@ const useGames=(gameQuery: GameQuery)=>
       },
       staleTime: ms("24h")
      });
+}
+
 
 
 export default useGames;
